f5563e9203b58ef54acc60217358b5c61ae32c3a
Natalia Barco <anbarco@unicauca.edu.co>
2021-10-26 10:25:10 -0500

    Finish 16th section

---
+++ 16th_section/start.rb
@@ -0,0 +1,8 @@
+puts "This is the begining"
+load "end.rb"
+puts "Alright, that was successful!"
+load "./end.rb"
+if 8 < 10
+  load "end.rb"
+end
+
---
+++ 16th_section/start2.rb
@@ -0,0 +1,9 @@
+puts "This is the beginning"
+require "./end"
+require "./end"
+require "./end"
+require "./end"
+require "./end"
+require_relative "end"
+require_relative "a/b/epilogue"
+
